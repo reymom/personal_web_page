3e0cc497b8825bad6db2c2c8231d4b4849c364c8
Ramon <ramon_gs95@hotmail.com>
2020-01-27 21:41:21 +0100

    [IMP] Improvements and Register Page, models

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,73 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_user, login_required, logout_user
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Reymon'}
     predictions = [
         {'author': {'username': 'Daniki'},
          'body': 'La escalada del año!'},
         {'author': {'username': 'Coto'},
          'body': 'Everydays psyco!'}
     ]
-    return render_template('index.html', title='Home', user=user, predictions=predictions)
+    return render_template('index.html', title='Home', predictions=predictions)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('index'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/services')
+@login_required
+def services():
+    return 'Private Services'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now my friend!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404
+    predictions = [
+        {'author': user, 'body': 'Prediction 1'},
+        {'author': user, 'body': 'Prediction 2'}
+    ]
+    return render_template('user.html', user=user, predictions=predictions)
