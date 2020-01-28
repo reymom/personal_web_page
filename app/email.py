a7ac9de831c8f381a4a93bbc8c37eca39d271b39
Ramon <ramon_gs95@hotmail.com>
2020-01-28 22:00:07 +0100

    [IMP] added reset password template and form

--- app/email.py
+++ app/email.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from flask_mail import Message
 from app import app, mail
+from threading import Thread
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 # https://pythonhosted.org/Flask-Mail/
@@ -8,7 +14,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
--- app/forms.py
+++ app/forms.py
@@ -54,3 +54,9 @@ class PredictionForm(FlaskForm):
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
